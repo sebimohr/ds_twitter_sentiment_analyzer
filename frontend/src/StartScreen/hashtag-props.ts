61ec0fa434ee731215d360286b462e15a450de4f
Sebastian Mohr <sebastianmohr96@gmail.com>
2023-01-05 15:02:34 +0100

    Changed props for hashtag and sentiment screen

--- frontend/src/StartScreen/hashtag-props.ts
+++ frontend/src/StartScreen/hashtag-props.ts
@@ -1,6 +1,8 @@
 export interface HashtagProps {
     hashtag: string
     setHashtag: Function
+    useCachedData: boolean
+    setUseCachedData: Function
     isShown: boolean
     changeIsShown: Function
 }
