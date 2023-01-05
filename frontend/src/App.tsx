8595375dd420d6353cef2b3e9e50610150a06d92
Sebastian Mohr <sebastianmohr96@gmail.com>
2023-01-05 15:03:20 +0100

    Added backdrop and error snackbar to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,51 @@ import React from "react";
 import ChooseEventHashtag from "./StartScreen/choose-event-hashtag";
 import ThemeHelper from "./Infrastructure/theme-helper";
 import SentimentScreen from "./SentimentScreen/sentiment-screen";
+import LoadingBackdrop from "./StartScreen/loading-backdrop";
+import axios from "axios";
+import ErrorSnackbar from "./StartScreen/error-snackbar";
+import { responsiveProperty } from "@mui/material/styles/cssUtils";
+
+const backendApiUrl = "http://127.0.0.1:9001";
+
+const sleep = (ms: number) => new Promise(
+    resolve => setTimeout(resolve, ms)
+);
 
 export default function App() {
-    const [hashtag, setHashtag] = React.useState<string>("");
+    const [hashtag, setHashtag] = React.useState<string>('');
+    const [useCachedData, setUseCachedData] = React.useState<boolean>(true);
     const [hashtagScreenIsShown, setHashtagScreenIsShown] = React.useState<boolean>(true);
-
+    const [loadingScreenIsShown, setLoadingScreenIsShown] = React.useState<boolean>(false);
+    const [errorSnackbarIsShown, setErrorSnackbarIsShown] = React.useState<boolean>(false);
+    const [errorSnackbarMessage, setErrorSnackbarMessage] = React.useState<string>('');
     const [sentimentScreenIsShown, setSentimentScreenIsShown] = React.useState<boolean>(false);
 
-    const hashtagScreenSubmission = () => {
+    const hashtagScreenSubmission = async () => {
+        setLoadingScreenIsShown(true);
+
+        /*await axios.get(backendApiUrl + '/api/sentiment', {
+            params: {
+                hashtag: hashtag,
+                cache: useCachedData
+            }
+        })
+            .then(function (response) {
+                setLoadingScreenIsShown(false);
+                setHashtagScreenIsShown(false);
+                setSentimentScreenIsShown(true);
+            })
+            .catch(function (_) {
+                setLoadingScreenIsShown(false);
+                setErrorSnackbarMessage("Couldn't retrieve data, please try again or inform your system administrator.")
+                setErrorSnackbarIsShown(true);
+                return;
+            })*/
+
+        await sleep(1000);
+
+
+        setLoadingScreenIsShown(false);
         setHashtagScreenIsShown(false);
         setSentimentScreenIsShown(true);
     }
@@ -21,12 +58,20 @@ export default function App() {
                 <ChooseEventHashtag
                     hashtag={hashtag}
                     setHashtag={setHashtag}
+                    useCachedData={useCachedData}
+                    setUseCachedData={setUseCachedData}
                     isShown={hashtagScreenIsShown}
                     changeIsShown={hashtagScreenSubmission}/>
                 <SentimentScreen
                     hashtag={hashtag}
                     isShown={sentimentScreenIsShown}
                     changeIsShown={setSentimentScreenIsShown}/>
+                <LoadingBackdrop
+                    showDialog={loadingScreenIsShown}/>
+                <ErrorSnackbar
+                    openErrorSnackbar={errorSnackbarIsShown}
+                    changeOpenErrorSnackbar={setErrorSnackbarIsShown}
+                    errorMessage={errorSnackbarMessage}/>
             </ThemeHelper>
         </div>
     );
