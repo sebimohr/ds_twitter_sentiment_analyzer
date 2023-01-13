0363db261647f584d87a14d2365ae163f35b7a6c
Sebastian Mohr <sebastianmohr96@gmail.com>
2023-01-13 19:14:39 +0100

    Fixed final bug

--- TwitterSentimentAnalyzer/main.py
+++ TwitterSentimentAnalyzer/main.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
 from WebApi.api_setup import ApiSetup
 
 if __name__ == '__main__':
+    cache_path = Path.joinpath(Path.cwd(), "Cache")
+    if not Path.exists(cache_path):
+        Path.mkdir(cache_path)
+    
     main_api = ApiSetup(9001)
     main_api.RegisterApiEndpoints().RunApi()
